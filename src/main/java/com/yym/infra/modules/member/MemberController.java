e854aebc770ae743d531bc807f3fbe6a3625e065
yym2044 <yym2044@gmail.com>
2022-03-31 22:39:52 +0900

    .

--- src/main/java/com/yym/infra/modules/member/MemberController.java
+++ src/main/java/com/yym/infra/modules/member/MemberController.java
@@ -6,14 +6,22 @@ import java.util.Map;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+@Controller
 public class MemberController {
 	
 	@Autowired
 	MemberServiceImpl service;
 	
+	
+	@RequestMapping(value="/member/member")
+	public String member() throws Exception {
+		
+		return "/member/member";
+	}
 	
 	@ResponseBody
 	@RequestMapping(value = "/member/loginProc")
