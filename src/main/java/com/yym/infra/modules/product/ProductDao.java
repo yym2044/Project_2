d470f907a543e24e7e9ec48caffd2a8afe5a6a8f
yym2044 <yym2044@gmail.com>
2022-04-01 00:09:30 +0900

    .

--- src/main/java/com/yym/infra/modules/product/ProductDao.java
+++ src/main/java/com/yym/infra/modules/product/ProductDao.java
@@ -1,5 +1,21 @@
 package com.yym.infra.modules.product;
 
+import java.util.List;
+
+import javax.inject.Inject;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.stereotype.Repository;
+
+@Repository
 public class ProductDao {
+	
+	@Inject
+//	@Resource(name = "sqlSession")
+	private SqlSession sqlSession;
+	
+	private static String namespace = "com.yym.infra.modules.product.ProductMpp";
+	
+	public List<Product> selectCategory() {return sqlSession.selectList(namespace + ".selectCategory", ""); }
 
 }
