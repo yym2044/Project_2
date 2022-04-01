7f21004c1134e2ff131c7baa06b6a23820af3edc
yym2044 <yym2044@gmail.com>
2022-04-01 20:27:06 +0900

    .

--- src/main/java/com/yym/infra/modules/product/ProductController.java
+++ src/main/java/com/yym/infra/modules/product/ProductController.java
@@ -2,6 +2,7 @@ package com.yym.infra.modules.product;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 
@@ -10,6 +11,12 @@ public class ProductController {
 	
 	@Autowired
 	ProductServiceImpl service;
+	
+	@RequestMapping(value = "/product/productSearch")
+	public String productSearch(Model model, ProductVo vo) throws Exception {
+		
+		return "/product/productSearch";
+	}
 	
 	@RequestMapping(value = "/product/productView")
 	public String productView(ProductVo vo) throws Exception {
