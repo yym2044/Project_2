2b9bd8543e104a9d2213e7c47b6e32a0f6dad10a
yym2044 <yym2044@gmail.com>
2022-04-02 11:06:05 +0900

    .

--- src/main/java/com/yym/infra/HomeController.java
+++ src/main/java/com/yym/infra/HomeController.java
@@ -7,11 +7,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class HomeController {
 	
-	@RequestMapping(value = "/home")
-	public String home(Model model) {
-		
-		return "/home";
-	}
 	
 	@RequestMapping(value = "/login/loginForm")
 	public String loginForm() {
