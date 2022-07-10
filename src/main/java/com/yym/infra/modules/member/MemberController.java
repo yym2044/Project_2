cefd64be55614609715495a4325ebf237a297b59
yym2044 <yym2044@gmail.com>
2022-07-11 08:37:32 +0900

    날짜 기본세팅 비활성화

--- src/main/java/com/yym/infra/modules/member/MemberController.java
+++ src/main/java/com/yym/infra/modules/member/MemberController.java
@@ -191,23 +191,23 @@ public class MemberController {
 			System.out.println("---");
 		}
 		
-		vo.setShOptionDate(vo.getShOptionDate() == null ? 1 : vo.getShOptionDate());
-		
-		vo.setShPeriod(vo.getShPeriod() == null ? 0 : vo.getShPeriod());
-		
-		if(vo.getShPeriod() == 0) {
-			vo.setShDateStart(vo.getShDateStart() == null ? UtilDateTime.calculateDayString(UtilDateTime.nowLocalDateTime(), Constants.DATE_INTERVAL, 1) : vo.getShDateStart());
-			vo.setShDateEnd(vo.getShDateEnd() == null ? UtilDateTime.nowString() : vo.getShDateEnd());
-		} else if(vo.getShPeriod() == 1) {
-			vo.setShDateStart(UtilDateTime.calculateDayString(UtilDateTime.nowLocalDateTime(), Constants.DATE_INTERVAL, 1));
-			vo.setShDateEnd(UtilDateTime.nowString());
-		} else if(vo.getShPeriod() == 2) {
-			vo.setShDateStart(UtilDateTime.calculateDayString(UtilDateTime.nowLocalDateTime(), -1, 2));
-			vo.setShDateEnd(UtilDateTime.nowString());
-		} else if(vo.getShPeriod() == 3) {
-			vo.setShDateStart(UtilDateTime.calculateDayString(UtilDateTime.nowLocalDateTime(), -1, 3));
-			vo.setShDateEnd(UtilDateTime.nowString());
-		}
+//		vo.setShOptionDate(vo.getShOptionDate() == null ? 1 : vo.getShOptionDate());
+//		
+//		vo.setShPeriod(vo.getShPeriod() == null ? 0 : vo.getShPeriod());
+//		
+//		if(vo.getShPeriod() == 0) {
+//			vo.setShDateStart(vo.getShDateStart() == null ? UtilDateTime.calculateDayString(UtilDateTime.nowLocalDateTime(), Constants.DATE_INTERVAL, 1) : vo.getShDateStart());
+//			vo.setShDateEnd(vo.getShDateEnd() == null ? UtilDateTime.nowString() : vo.getShDateEnd());
+//		} else if(vo.getShPeriod() == 1) {
+//			vo.setShDateStart(UtilDateTime.calculateDayString(UtilDateTime.nowLocalDateTime(), Constants.DATE_INTERVAL, 1));
+//			vo.setShDateEnd(UtilDateTime.nowString());
+//		} else if(vo.getShPeriod() == 2) {
+//			vo.setShDateStart(UtilDateTime.calculateDayString(UtilDateTime.nowLocalDateTime(), -1, 2));
+//			vo.setShDateEnd(UtilDateTime.nowString());
+//		} else if(vo.getShPeriod() == 3) {
+//			vo.setShDateStart(UtilDateTime.calculateDayString(UtilDateTime.nowLocalDateTime(), -1, 3));
+//			vo.setShDateEnd(UtilDateTime.nowString());
+//		}
 		
 		int count = service.selectListCount(vo);
 		
@@ -389,23 +389,23 @@ public class MemberController {
 		System.out.println("UtilDateTime.nowDate : " + UtilDateTime.nowDate());
 		System.out.println("UtilDateTime.nowString() : " + UtilDateTime.nowString());
 
-		vo.setShOptionDate(vo.getShOptionDate() == null ? 1 : vo.getShOptionDate());
-		
-		vo.setShPeriod(vo.getShPeriod() == null ? 0 : vo.getShPeriod());
-		
-		if(vo.getShPeriod() == 0) {
-			vo.setShDateStart(vo.getShDateStart() == null ? UtilDateTime.calculateDayString(UtilDateTime.nowLocalDateTime(), Constants.DATE_INTERVAL, 1) : vo.getShDateStart());
-			vo.setShDateEnd(vo.getShDateEnd() == null ? UtilDateTime.nowString() : vo.getShDateEnd());
-		} else if(vo.getShPeriod() == 1) {
-			vo.setShDateStart(UtilDateTime.calculateDayString(UtilDateTime.nowLocalDateTime(), Constants.DATE_INTERVAL, 1));
-			vo.setShDateEnd(UtilDateTime.nowString());
-		} else if(vo.getShPeriod() == 2) {
-			vo.setShDateStart(UtilDateTime.calculateDayString(UtilDateTime.nowLocalDateTime(), -1, 2));
-			vo.setShDateEnd(UtilDateTime.nowString());
-		} else if(vo.getShPeriod() == 3) {
-			vo.setShDateStart(UtilDateTime.calculateDayString(UtilDateTime.nowLocalDateTime(), -1, 3));
-			vo.setShDateEnd(UtilDateTime.nowString());
-		}
+//		vo.setShOptionDate(vo.getShOptionDate() == null ? 1 : vo.getShOptionDate());
+//		
+//		vo.setShPeriod(vo.getShPeriod() == null ? 0 : vo.getShPeriod());
+//		
+//		if(vo.getShPeriod() == 0) {
+//			vo.setShDateStart(vo.getShDateStart() == null ? UtilDateTime.calculateDayString(UtilDateTime.nowLocalDateTime(), Constants.DATE_INTERVAL, 1) : vo.getShDateStart());
+//			vo.setShDateEnd(vo.getShDateEnd() == null ? UtilDateTime.nowString() : vo.getShDateEnd());
+//		} else if(vo.getShPeriod() == 1) {
+//			vo.setShDateStart(UtilDateTime.calculateDayString(UtilDateTime.nowLocalDateTime(), Constants.DATE_INTERVAL, 1));
+//			vo.setShDateEnd(UtilDateTime.nowString());
+//		} else if(vo.getShPeriod() == 2) {
+//			vo.setShDateStart(UtilDateTime.calculateDayString(UtilDateTime.nowLocalDateTime(), -1, 2));
+//			vo.setShDateEnd(UtilDateTime.nowString());
+//		} else if(vo.getShPeriod() == 3) {
+//			vo.setShDateStart(UtilDateTime.calculateDayString(UtilDateTime.nowLocalDateTime(), -1, 3));
+//			vo.setShDateEnd(UtilDateTime.nowString());
+//		}
 		
 		int count = service.selectListCount(vo);
 		
